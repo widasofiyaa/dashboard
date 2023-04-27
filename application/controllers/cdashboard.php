e75cc963d5a4963f6b97d5b98e06b87992df0fff
widasofiyaa <wydaun@student.telkomuniversity.ac.id>
2023-04-27 09:03:21 +0700

    fix-model-total-data-kelengkapan-pddikti

--- application/controllers/cdashboard.php
+++ application/controllers/cdashboard.php
@@ -23,7 +23,7 @@ class cdashboard extends CI_Controller {
 		parent::__construct();
 		$this->load->Model('Manomali');
 		$this->load->Model('Mpddikti_awards');
-		$this->load->Model('Mdata');
+		$this->load->Model('Mdata_kelengkapan_pddikti');
 	}
 	public function index()
 	{
@@ -31,9 +31,7 @@ class cdashboard extends CI_Controller {
 		if (isset($sess)) {
 			$data['total_dataanomali'] = $this->Manomali->total_dataanomali();
 			$data['total_data_pddikti_awards'] = $this->Mpddikti_awards->total_data_pddikti_awards();
-			$data['total_barang'] = $this->Mdata->total_barang();
-			$data['total_barang_kosong'] = $this->Mdata->total_barang_kosong();
-			$data['barang_habis'] = $this->Mdata->barang_habis()->result();
+			$data['total_data_kelengkapan_pddikti'] = $this->Mdata_kelengkapan_pddikti->total_data_kelengkapan_pddikti();
 
 		$this->load->view('head');
 		$this->load->view('top_bar');
